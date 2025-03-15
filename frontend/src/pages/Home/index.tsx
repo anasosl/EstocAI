e4c7be87abdf0526f8711d2041abe4044eddf4fa
Charles <cmsa@cin.ufpe.br>
2025-03-15 16:48:25 -0300

    feat(navbar): add Footer to Components

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Navbar } from '../../components/Navbar';
-import { Button, InputAdornments } from '../../components';
+import { Navbar, Footer, Button, InputAdornments } from '../../components';
 import { theme } from '../../styles/theme';
-import { Footer } from '../../components/Footer';
 
 export const Home: React.ElementType = () => {
 	const [value, setValue] = React.useState('');
