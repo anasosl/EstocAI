d61fe48d66edbb4a14417b0ebe40e140b6eefe78
Wellinne <wellinnenascimento@gmail.com>
2025-03-16 19:53:14 -0300

    deletando node_modules

--- frontend/src/pages/Cadastro/index.tsx
+++ frontend/src/pages/Cadastro/index.tsx
@@ -2,21 +2,45 @@ import React from "react";
 import assets from '../../assets'; // Importar a imagem
 import { BackgroundImage, PageContainer, StorangeOrangeBackground } from "./style";
 import { StorangeOrangeLogo } from "../Login/style";
+import { InputAdornments } from "../../components";
+import { caracterCustomizado } from "../../utils/Regex/masks";
 
 
 export const Cadastro: React.FC = () => {
-  
+  const [userData, setUserData] = React.useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    birthDate: '',
+    cpf: '',
+    phone: '',
+  });
+  const [errorMessages, setErrorMessages] = React.useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    birthDate: '',
+    cpf: '',
+    phone: '',
+  });
+
   return (
     <PageContainer>
         <StorangeOrangeLogo src={assets.storangeOrangeLogo} alt="Logo" />
         <h3>Acesse sua conta</h3>
-        <form>
-          <input type="text" placeholder="Número de telefone ou e-mail" />
-          <input type="password" placeholder="Senha" />
-          <button >Entrar</button>
+        <InputAdornments
+          id="input-nome"
+          placeholder="Nome"
+          value={userData.name}
+          setValue={(text: any) => setUserData({ ...userData, name: caracterCustomizado(text, 50) })}
+          type="text"
+          width="100%"
+          errorMessage={errorMessages.name}
+        />
       
       
-        </form>
       <StorangeOrangeBackground src={assets.storangeOrangeBackground}/>
     </PageContainer>
   );
